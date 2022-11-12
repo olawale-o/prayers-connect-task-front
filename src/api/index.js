0dc480f48825e71c141202870151acc4673a72fe
olawale-o <omoogunolawale@gmail.com>
2022-11-12 21:34:39 +0100

    Create http verbs

---
+++ src/api/index.js
@@ -0,0 +1,44 @@
+const BASE_URI = 'http://localhost:5000/api/v1/';
+
+const INIT = {
+  method: 'GET',
+  headers: {
+    'Content-type': 'application/json'
+  },
+  mode: 'cors',
+};
+
+export const post = async (endpoint, payload) => {
+  const response = await fetch(`${BASE_URI}${endpoint}`, {
+    ...INIT,
+    method: 'POST',
+    body: JSON.stringify(payload)
+  });
+  if (!response.ok) {
+    throw new Error('Failed to submit request');
+  }
+  const data = await response.json();
+  return data;
+};
+
+export const get = async (endpoint) => {
+  const response = await fetch(`${BASE_URI}${endpoint}`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch request');
+  }
+  const data = await response.json();
+  return data.todos;
+};
+
+export const put = async (endpoint, payload) => {
+  const response = await fetch(`${BASE_URI}${endpoint}`, {
+    ...INIT,
+    method: 'PUT',
+    body: JSON.stringify(payload)
+  });
+  if (!response.ok) {
+    throw new Error('Failed to submit request');
+  }
+  const data = await response.json();
+  return data;
+};
