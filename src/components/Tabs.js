98fd9f6591f8eef8dd1728a9e3467485fcecc0e1
olawale-o <omoogunolawale@gmail.com>
2022-11-13 17:58:28 +0100

    Update Tabs component

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -5,15 +5,9 @@ const Tabs = ({ switchTabs, tabIndex }) => {
       <ul className="tabs">
         {tabs.map((tab) => (
           <li key={tab.index} className={`tab-item ${tabIndex === tab.index && 'active'}`}>
-            <button className="tab-button" onClick={() => switchTabs(tab.index)}>{tab.title}{tab.index}</button>
+            <button className="tab-button" onClick={() => switchTabs(tab.index)}>{tab.title}</button>
           </li>
         ))}
-        {/* <li className={`tab-item ${tabIndex === 1 && 'active'}`}>
-          <button className="tab-button" onClick={() => switchTabs(1)}>In-Progress</button>
-        </li>
-        <li className={`tab-item ${tabIndex === 2 && 'active'}`}>
-          <button className="tab-button" onClick={() => switchTabs(2)}>Done</button>
-        </li> */}
       </ul>
     </div>
   )
