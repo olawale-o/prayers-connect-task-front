dc912b215daa30266d5f767d80692c5fea5c24e6
olawale-o <omoogunolawale@gmail.com>
2022-11-13 15:29:09 +0100

    Format layout

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import TaskForm from "./components/TaskForm";
 function App() {
   return (
     <div className="App">
-        <TaskProvider>
-          <TaskList />
-          <TaskForm />
-        </TaskProvider>
+      <TaskProvider>
+        <TaskList />
+        <TaskForm />
+      </TaskProvider>
     </div>
   );
 }
