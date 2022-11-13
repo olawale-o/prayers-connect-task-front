db15ace7968552609557fb0d0ecec7c3504a2aaa
olawale-o <omoogunolawale@gmail.com>
2022-11-13 17:59:58 +0100

    Update components

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,16 +1,72 @@
 import React from 'react';
 import useTask from '../hooks/useTask';
+import useTaskDispatch from '../hooks/useTaskDispatch';
 import Task from './Task';
+import Tabs from './Tabs';
+import TaskService from '../services/task';
 
 const TaskList = () => {
-  const tasks = useTask();
-  if (tasks.length < 1) return <div>Loading</div>
+  const tabs = ['todo', 'in-progress', 'done'];
+  const taskObj = useTask();
+  const dispatch = useTaskDispatch();
+  const [tabIndex, setTabIndex] = React.useState(1);
+  const [filter, setFilter] = React.useState(tabs[0]);
+  const switchTabs = (position) => {
+    dispatch({ type: 'loading' })
+    setTabIndex(position);
+    setFilter(tabs[position - 1]);
+  };
+
+  React.useEffect(() => {
+    dispatch({ type: 'loading' })
+    TaskService.getAllTasks(filter)
+    .then((response) => {
+      console.log(response)
+      dispatch({ type: 'init', payload: response })
+    }).catch((err) => dispatch({ type: 'error' }));
+  }, [filter]);
   return (
     <div className="task-container p-2">
-      <ul className="task-list">
-        {tasks.map((task) => (<Task task={task} key={task.id} />))}
-      </ul>
+    <div className="tab-container">
+      <Tabs switchTabs={switchTabs} tabIndex={tabIndex} />
+      {taskObj?.loading ? (<div className="loading" />)
+        : (
+          <div className="tab-view">
+            {tabIndex === 1 && (
+              <div>
+                {taskObj.tasks.length < 1 ? 
+                  (<p className="helper-text">No pending tasks</p>) :
+                  (<ul className="task-list">
+                    {taskObj.tasks.map((task) => (<Task task={task} key={task.id} />))}
+                  </ul>)
+                }
+              </div>
+            )}
+            {tabIndex === 2 && (
+              <div>
+                {taskObj.tasks.length < 1 ? 
+                  (<p className="helper-text">There are no pending tasks</p>) :
+                  (<ul className="task-list">
+                    {taskObj.tasks.map((task) => (<Task task={task} key={task.id} />))}
+                  </ul>)
+                }
+              </div>
+            )}
+            {tabIndex === 3 && (
+              <div>
+                {taskObj.tasks.length < 1 ? 
+                  (<p className="helper-text">There are no completed tasks</p>) :
+                  (<ul className="task-list">
+                    {taskObj.tasks.map((task) => (<Task task={task} key={task.id} />))}
+                  </ul>)
+                }
+              </div>
+            )}
+          </div>
+        )
+      }
     </div>
+  </div>
   );
 }
 
