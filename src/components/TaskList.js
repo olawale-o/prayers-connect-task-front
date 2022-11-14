d1506ebf0b11cc3b4d04a5734f719302a02885b6
olawale-o <omoogunolawale@gmail.com>
2022-11-14 14:17:26 +0100

    Modify TaskList component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,67 +1,36 @@
 import React from 'react';
 import useTask from '../hooks/useTask';
 import useTaskDispatch from '../hooks/useTaskDispatch';
-import Task from './Task';
 import Tabs from './Tabs';
 import TaskService from '../services/task';
+import TaskListView from './TaskListView';
 
 const TaskList = () => {
   const tabs = ['todo', 'in-progress', 'done'];
-  const taskObj = useTask();
-  const dispatch = useTaskDispatch();
+  const { loading, tasks } = useTask();
+  const { setLoading, getTasks, setError } = useTaskDispatch();
   const [tabIndex, setTabIndex] = React.useState(1);
   const [filter, setFilter] = React.useState(tabs[0]);
   const switchTabs = (position) => {
-    dispatch({ type: 'loading' })
     setTabIndex(position);
     setFilter(tabs[position - 1]);
   };
 
   React.useEffect(() => {
-    dispatch({ type: 'loading' })
+    setLoading();
     TaskService.getAllTasks(filter)
     .then((response) => {
-      console.log(response)
-      dispatch({ type: 'init', payload: response })
-    }).catch((err) => dispatch({ type: 'error' }));
+      getTasks(response)
+    }).catch((err) => setError('error'));
   }, [filter]);
   return (
     <div className="task-container p-2">
     <div className="tab-container">
       <Tabs switchTabs={switchTabs} tabIndex={tabIndex} />
-      {taskObj?.loading ? (<div className="loading" />)
+      {loading ? (<div className="loading" />)
         : (
           <div className="tab-view">
-            {tabIndex === 1 && (
-              <div>
-                {taskObj.tasks.length < 1 ? 
-                  (<p className="helper-text">No pending tasks</p>) :
-                  (<ul className="task-list">
-                    {taskObj.tasks.map((task) => (<Task task={task} key={task.id} />))}
-                  </ul>)
-                }
-              </div>
-            )}
-            {tabIndex === 2 && (
-              <div>
-                {taskObj.tasks.length < 1 ? 
-                  (<p className="helper-text">There are no pending tasks</p>) :
-                  (<ul className="task-list">
-                    {taskObj.tasks.map((task) => (<Task task={task} key={task.id} />))}
-                  </ul>)
-                }
-              </div>
-            )}
-            {tabIndex === 3 && (
-              <div>
-                {taskObj.tasks.length < 1 ? 
-                  (<p className="helper-text">There are no completed tasks</p>) :
-                  (<ul className="task-list">
-                    {taskObj.tasks.map((task) => (<Task task={task} key={task.id} />))}
-                  </ul>)
-                }
-              </div>
-            )}
+            <TaskListView tab={filter} tasks={tasks} />
           </div>
         )
       }
