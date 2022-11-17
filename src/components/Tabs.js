0248a70d646525a5f917d8539beeed157a94e0f3
olawale-o <omoogunolawale@gmail.com>
2022-11-18 00:00:06 +0100

    Improve web accessibility

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,11 +1,19 @@
 const Tabs = ({ switchTabs, tabIndex }) => {
   const tabs = [{ index: 1, title: 'Todo' }, { index: 2, title: 'In-Progress'}, { index: 3, title: 'Done'}];
   return (
-    <div className="tab-bar">
+    <div className="tab-bar" role="tablist" aria-label="Status">
       <ul className="tabs">
         {tabs.map((tab) => (
           <li key={tab.index} className={`tab-item ${tabIndex === tab.index && 'active'}`}>
-            <button className="tab-button" onClick={() => switchTabs(tab.index)}>{tab.title}</button>
+            <button
+              className="tab-button"
+              onClick={() => switchTabs(tab.index)}
+              role="tab"
+              aria-selected={tabIndex === tab.index && true}
+              aria-controls={`tab-${tab.index}`}
+            >
+              {tab.title}
+              </button>
           </li>
         ))}
       </ul>
