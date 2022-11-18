9d8d4e4f5b541d99a811b1985a3cac286df7e5cb
olawale-o <omoogunolawale@gmail.com>
2022-11-18 08:02:08 +0100

    Improve web accessibility

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,13 +4,30 @@ const Modal = ({ modal, closeModal }) => {
   if (!modal?.isOpen) return null;
   return (
     ReactDOM.createPortal(
-      <button className={`modal ${modal?.isOpen && 'show'}`} onClick={closeModal}>
-        <div className="modal-background">
+      <button
+        className={`modal ${modal?.isOpen && 'show'}`}
+        onClick={closeModal}
+      >
+        <div
+          className="modal-background"
+          aria-modal="true"
+          role="alertdialog"
+          aria-describedby="dialog-desc"
+          aria-labelledby="dialog-label"
+        >
           <div className={`modal-content ${modal?.isOpen && 'show'}`}>
             <div className="modal-close__btn">
-              <button className="close-btn" type="buttton" onClick={closeModal}>X</button>
+              <button
+                className="close-btn"
+                type="buttton"
+                onClick={closeModal}
+                aria-label="close"
+              >
+                X
+              </button>
             </div>
-            <p className="modal-text">{modal?.message}</p>
+            <h2 className="modal-label" id="dialog-label">Message</h2>
+            <p className="modal-text" id="dialog-desc">{modal?.message}</p>
           </div>
         </div>
       </button>,
