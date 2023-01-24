fffac2e781d242a3acfe6fa9ba7d653f8535c7c3
olawale-o <omoogunolawale@gmail.com>
2023-01-24 06:57:23 +0100

    Modify modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,10 @@
 import ReactDOM from "react-dom";
 
-const Modal = ({ modal, closeModal }) => {
-  if (!modal?.isOpen) return null;
+const Modal = ({ isOpen, message, closeModal }) => {
   return (
     ReactDOM.createPortal(
-      <button
-        className={`modal ${modal?.isOpen && 'show'}`}
-        onClick={closeModal}
+      <div
+        className={`modal ${isOpen && 'show'}`}
       >
         <div
           className="modal-background"
@@ -15,11 +13,11 @@ const Modal = ({ modal, closeModal }) => {
           aria-describedby="dialog-desc"
           aria-labelledby="dialog-label"
         >
-          <div className={`modal-content ${modal?.isOpen && 'show'}`}>
+          <div className={`modal-content ${isOpen && 'fade-in'}`}>
             <div className="modal-close__btn">
               <button
                 className="close-btn"
-                type="buttton"
+                type="button"
                 onClick={closeModal}
                 aria-label="close"
               >
@@ -27,10 +25,10 @@ const Modal = ({ modal, closeModal }) => {
               </button>
             </div>
             <h2 className="modal-label" id="dialog-label">Message</h2>
-            <p className="modal-text" id="dialog-desc">{modal?.message}</p>
+            <p className="modal-text" id="dialog-desc">{message}</p>
           </div>
         </div>
-      </button>,
+      </div>,
     document.body
    )
   );
