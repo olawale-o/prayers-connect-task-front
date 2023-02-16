de2f957f521021994e140c476672af2bc07ac4b7
olawale-o <omoogunolawale@gmail.com>
2023-02-16 02:45:42 +0100

    Use outlet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import { Outlet } from "react-router-dom";
 import TaskProvider from "./providers/TaskProvider";
-import TaskList from "./components/TaskList";
-import TaskForm from "./components/TaskForm";
 
 function App() {
   return (
     <div className="App">
       <TaskProvider>
-        <TaskList />
-        <TaskForm />
+        <Outlet />
       </TaskProvider>
     </div>
   );
