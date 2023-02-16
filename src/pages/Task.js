2f3de1b9da12f0bb7cb300a4b49042ec9ce501a6
olawale-o <omoogunolawale@gmail.com>
2023-02-16 02:46:11 +0100

    Create task page

---
+++ src/pages/Task.js
@@ -0,0 +1,13 @@
+import TaskList from "../components/TaskList";
+import TaskForm from "../components/TaskForm";
+
+function Task() {
+  return (
+    <>
+      <TaskList />
+      <TaskForm />
+    </>
+  );
+}
+
+export default Task;
