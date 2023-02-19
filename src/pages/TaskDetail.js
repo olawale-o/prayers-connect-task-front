7310ea439c1a305044859e609ced6d68ed8b8b3e
olawale-o <omoogunolawale@gmail.com>
2023-02-19 10:10:12 +0100

    Modify task details page

--- src/pages/TaskDetail.js
+++ src/pages/TaskDetail.js
@@ -1,10 +1,32 @@
-import { useParams } from "react-router-dom";
+import React, { useEffect, useCallback } from "react";
+import { useParams, useLoaderData } from "react-router-dom";
+
+import { useModals } from '../hooks/useModals';
+import TaskEditModal from "../components/TaskEditModal";
+
+import TaskService from "../services/task";
+
+export async function loader({ params }) {
+  return TaskService.getTask(params.id);
+}
 
 const TaskDetail = () => {
   const { id } = useParams();
+  const data = useLoaderData();
+  const [openEditModal, closeEditModal, editModal] = useModals('EditModal');
 
+  const openModal = useCallback(() => openEditModal(), [id]);
+
+  useEffect(() => {
+    openModal();
+  }, [openModal]);
   return (
-    <div>Task {id}</div>
+    <TaskEditModal
+      isOpen={editModal.EditModal}
+      closeModal={closeEditModal}
+      task={data}
+      index={id}
+    />
   )
 };
 
